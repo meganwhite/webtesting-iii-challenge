40101dab36406cb8d2bff7d220df3f25e11d56b2
Megan White <mfw247@gmail.com>
2019-08-21 07:07:06 -0700

    install dependencies and add controls tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,36 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+
+import Controls from "./Controls"
+
+describe("<Controls />", () => {
+    it('should disable lock button if gate is open', () => {
+        //render copy of control component
+        const wrapper = render(<Controls />);
+        const lockButton = wrapper.getByText("Lock Gate")
+        //look for property on control component
+        expect(lockButton).toBeDisabled();
+    })
+
+    it('should disable open button if gate is closed and locked', () => {
+        //when it renders Controls, it will render in this state
+        const {getByText} = render(<Controls locked={true} closed={true}/>)
+        //target the button
+        const openButton = getByText(/open gate/i);
+        expect(openButton.disabled).toBe(true);
+
+    })
+
+    it('should not call lock function if gate is open', () => {
+        const mockLock = jest.fn();
+        const {getByText} = render(<Controls />);
+        const lockButton = getByText(/lock gate/i);
+        //look to see if function fired
+        fireEvent.click(lockButton);
+        expect(mockLock).not.toBeCalled();
+    })
+
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Display from './Display';
+
+describe("Test to see if classes are set properly", () => {
+    it('should have class "green-led" when props are false', () => {
+        const {getByText} = render(<Display closed = {false} locked={false}/>);
+        const unlocked = getByText(/unlocked/i);
+        const open = getByText(/open/i);
+        expect(unlocked.classList.contains("green-led")).toBe(true);
+        expect(open).toHaveClass("green-led");
+    })
+
+    it('should have class "red-led" when props are false', () => {
+        const {getByText} = render(<Display closed = {true} locked={true}/>);
+        const locked = getByText(/locked/i);
+        const closed = getByText(/closed/i);
+        expect(locked.classList.contains("red-led")).toBe(true);
+        expect(closed).toHaveClass("red-led");
+    })
+
+})
